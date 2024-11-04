1a7bc03ecf2ef43f959aac440b261ec7b7cc76c5
vsctmp17 <vsctmp17@k28i14.tier2.hpc.kuleuven.be>
2024-11-04 10:39:44 +0100

    Fixed script.R

--- exercises/R/script.R
+++ exercises/R/script.R
@@ -1,12 +1,17 @@
-setwd("../../montesmariana/r-best-practices-exercises/data")
+# 1. Set working directory ====
+getwd()
 
-df<-read.csv( "Flight Subset 2013.csv")
+# 2. Load data ====
+df<-read.csv( "exercises/data/Flight Subset 2013.csv")
+
+# 3. Variable changes ====
 df$month_name = month.name[df$month]
 df$carrier <- as.factor(df$carrier)
 df$tailnum <- as.factor(df$tailnum)
 df$origin <- as.factor(df$origin)
+
 for(i in 1:length(df$dep_delay)){
-if(is.na(df$dep_delay[[i]])){
+if(is.na(df$dep_delay[[i]])== T){
 df[i, "dep_delay_cat"] <- NA
 }else if(df$dep_delay[[i]] < -30){
     df[i, "dep_delay_cat"] <- delay_categories[[1]]
@@ -16,5 +21,6 @@ df[i, "dep_delay_cat"] <- NA
     df[i, "dep_delay_cat"] <- delay_categories[[3]]
   }
 }
+
 df$dep_delay_cat <- as.factor(df$dep_delay_cat)
 delay_categories <- c("Early", "Kind of on time", "Late")
